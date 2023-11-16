8f9e289af471923ad565b74d8639a4e93ba15863
Naheed <89418276+naheedtayab@users.noreply.github.com>
2023-11-16 21:36:54 +0000

    Initial Commit

---
+++ Workplace Health Monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workplace Health Monitor
+//
+//  Created by Naheed on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workplace Health Monitor/Workplace_Health_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workplace_Health_MonitorApp.swift
+//  Workplace Health Monitor
+//
+//  Created by Naheed on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Workplace_Health_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
